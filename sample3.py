5b93ed67320cd8fe03f3a6c8efcbce1e7a7593df
gagan123-ys <punithpunibm@gmail.com>
2025-10-24 23:06:39 +0530

    add sample3.py

---
+++ sample3.py
@@ -0,0 +1,11 @@
+def greet(name):
+    print("Hello, " + name) 
+def unused_function():       
+    pass
+
+class myClass:               
+    def __init__(self, value):
+        self.value = value
+
+if __name__ == "__main__":
+    greet(123)               
