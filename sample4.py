7068993f1e03a0b5ceba38f898b74c6a6c29a1d5
darshanbn-bn <darshanbndarshan83@gmail.com>
2025-11-10 22:07:53 +0530

    add sample4.py

---
+++ sample4.py
@@ -0,0 +1,12 @@
+def green(name):
+    print("Hello, " + name)
+
+def unused_function():
+    pass
+
+class myclass:
+    def __init__(self, value):
+        self.value = value
+
+if __name__ "__main__":
+    greet(123)
